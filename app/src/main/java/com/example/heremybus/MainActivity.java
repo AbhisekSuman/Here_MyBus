f600dcaabd81e40f297aa74e9f2b242bc3a9e18e
Om Prasad Panda <omprasadpandadell@gmail.com>
2022-10-16 16:45:34 +0530

    Updated

--- app/src/main/java/com/example/heremybus/MainActivity.java
+++ app/src/main/java/com/example/heremybus/MainActivity.java
@@ -5,22 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
-    private View helo;
+    private View busview1;
+    LottieAnimationView animationView;
+    ImageView imageView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        animationView = findViewById(R.id.clickanimation);
+        imageView = findViewById(R.id.image1);
 
-        helo=findViewById(R.id.Helo);
-        helo.setOnClickListener(new View.OnClickListener() {
+
+        setContentView(R.layout.activity_main);
+        busview1=findViewById(R.id.image1);
+        busview1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this,Bus1Driver.class);
-                startActivity(intent);
+                animationView.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.VISIBLE);
+                animationView.playAnimation();
+
+                Thread td = new Thread(){
+
+                    public void run(){
+                        try {
+                            sleep(
+                                    100
+                            );
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        finally {
+                            Intent intent =new Intent(MainActivity.this,Bus1UserMaps.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                };td.start();
+
             }
         });
 
