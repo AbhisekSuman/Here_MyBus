aa74f6dc1e88aff144c1a1e37e437ea4f7b7aa4e
Abhisek Nayak <cv07abhisek@gmail.com>
2022-10-16 13:19:54 +0530

    Maps are loaded

--- app/src/main/java/com/example/heremybus/MainActivity.java
+++ app/src/main/java/com/example/heremybus/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.heremybus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private View helo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        helo=findViewById(R.id.Helo);
+        helo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Bus1Driver.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
