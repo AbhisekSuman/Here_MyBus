5aee5ba50a8d970a2ca471ee989766abad625862
Om Prasad Panda <omprasadpandadell@gmail.com>
2022-10-16 17:01:07 +0530

    Updated

--- app/src/main/java/com/example/heremybus/MainActivity.java
+++ app/src/main/java/com/example/heremybus/MainActivity.java
@@ -5,15 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
     private View busview1;
-    LottieAnimationView animationView;
-    ImageView imageView;
-
 
 
 
@@ -22,37 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        animationView = findViewById(R.id.clickanimation);
-        imageView = findViewById(R.id.image1);
-
 
         setContentView(R.layout.activity_main);
         busview1=findViewById(R.id.image1);
         busview1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                animationView.setVisibility(View.VISIBLE);
-                imageView.setVisibility(View.VISIBLE);
-                animationView.playAnimation();
-
-                Thread td = new Thread(){
-
-                    public void run(){
-                        try {
-                            sleep(
-                                    100
-                            );
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        finally {
-                            Intent intent =new Intent(MainActivity.this,Bus1UserMaps.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    }
-                };td.start();
-
+                Intent intent =new Intent(MainActivity.this,Bus1UserMaps.class);
+                startActivity(intent);
             }
         });
 
